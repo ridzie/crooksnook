75297b9bc9f3ceea1b5e7c9de37ce5245ec88475
Ridhima <edu.ridhima@gmail.com>
2025-09-05 18:22:44 +0200

    trying to switch from static to fullstack

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,19 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
 
+/*
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+});
+*/
+
+export default defineConfig({
+  output: "server",                 // not "static"
+  adapter: node({ mode: "standalone" }), // runs as Node app
+  site: 'https://ridzie.me',
+  integrations: [mdx(), sitemap()],
 });
